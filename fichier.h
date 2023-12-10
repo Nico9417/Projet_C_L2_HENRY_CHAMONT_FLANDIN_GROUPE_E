5482cb9e5dd2fdd1bc4bc1b003067f4838e4f13a
Maxiiime78 <124362575+Maxiiime78@users.noreply.github.com>
2023-12-10 15:57:39 +0100

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 typedef struct s_sk_cell
 {
     int value;
@@ -14,8 +13,10 @@ typedef struct s_sk_list
     int max_cell;
 } t_sk_list;
 
+//Nos fonctions
 
 t_sk_list *create_list(int max_level);
+
 t_sk_cell* CreateCell (int, int);
 
 t_sk_list CreateEmptyList (int);
@@ -23,8 +24,13 @@ t_sk_list CreateEmptyList (int);
 void insertHead(t_sk_list *, int, int);
 
 void display_list(t_sk_list* list, int level);
+
 void displayAlllist(t_sk_list* list);
+
 void insertList(t_sk_list* liste, int valeur, int level);
+
 int isInList(t_sk_list* liste, int level, int valeur);
+
 void isInlistAll(t_sk_list *liste, int valeur);
+
 void searchligne(int valeur, t_sk_list *liste, int n);
